f025febdd64b87e9a31925c50c3443ac2c0757a3
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2024-12-04 21:49:49 -0300

    continue from astro-auth project

---
+++ db/seed.ts
@@ -0,0 +1,40 @@
+
+import { v4 as UUID } from 'uuid';
+import bcrypt from 'bcryptjs';
+import { db, Role, User } from 'astro:db';
+
+
+export default async function seed() {
+
+
+	const roles = [
+		{
+			id: 'admin',
+			name: 'Admin'
+		},
+		{
+			id: 'user',
+			name: 'User'
+		}
+	]
+
+	const johnDoe = {
+		id: UUID(),
+		name: 'John Doe',
+		email: 'john.doe@example.com',
+		password: bcrypt.hashSync('123456'),
+		role: 'admin'
+	}
+
+	const janeDoe = {
+		id: UUID(),
+		name: 'Jane Doe',
+		email: 'jane.doe@example.com',
+		password: bcrypt.hashSync('123456'),
+		role: 'user'
+	}
+
+	await db.insert(Role).values(roles);
+	await db.insert(User).values([johnDoe, janeDoe]);
+
+}
