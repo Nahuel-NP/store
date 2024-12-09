8d13b7d47eccd82422c74171c2860c7abde38b6a
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2024-12-09 15:46:24 -0300

    create product with images interface

---
+++ src/interfaces/product-with-image.interface.ts
@@ -0,0 +1,15 @@
+
+export interface ProductWithImage  {
+  id: string;
+  description: string;
+  gender: string;
+  price: number;
+  sizes: string;
+  slug: 'men_plaid_mode_tee',
+  stock: number;
+  tags: string;
+  title: string;
+  type: string;
+  user: string;
+  images: string;
+}
