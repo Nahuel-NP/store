3678af3a43028fb39280345ef4d3c3df1df0a68a
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2024-12-18 11:20:54 -0300

    create new product by slug new

--- src/actions/products/get-product-by-slug.action.ts
+++ src/actions/products/get-product-by-slug.action.ts
@@ -4,10 +4,28 @@ import { defineAction } from "astro:actions"
 import { db, eq, Product, ProductImage } from "astro:db";
 import { z } from "astro:schema"
 
+const newProduct = {
+  id:'',
+  description: 'Nueva descripción',
+  gender: 'men',
+  price: 12,
+  sizes: 'M',
+  slug: 'nuevo-producto',
+  stock: 4,
+  tags: 'shirt,men',
+  title: 'new product',
+  type: 'shirts',
+}
+
 export const getProductBySlugAction = defineAction({
   accept: 'json',
   input: z.string(),
   handler: async (slug) => {
+
+    if (slug === 'new') {
+      return { product: newProduct, images: [] };
+    }
+
     const [product] = await db.select().from(Product).where(eq(Product.slug, slug));
     if (!product) {
       throw new Error('Product not found');
