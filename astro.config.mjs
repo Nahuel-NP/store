68859af082ac1d654498935a37f98b6f2f63b897
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2024-12-31 14:13:18 -0300

    add env schema

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
-import db from '@astrojs/db';
+import db from "@astrojs/db";
 
-import auth from 'auth-astro';
+import auth from "auth-astro";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), db(), auth(), react()],
   adapter: netlify(),
-  output: 'server',
+  output: "server",
+  env: {
+    schema: {
+      CLOUDINARY_CLOUD_NAME: envField.string({
+        access: "public",
+        context: "server",
+      }),
+      CLOUDINARY_API_SECRET: envField.string({
+        access: "secret",
+        context: "server",
+      }),
+      CLOUDINARY_API_KEY: envField.string({
+        access: "public",
+        context: "server",
+      }),
+    },
+  },
 });
